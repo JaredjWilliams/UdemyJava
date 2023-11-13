b97ec1ca4851b05c5ef33c686626093b3b81558b
Jared Williams <jared.williams101697@gmail.com>
2023-11-12 21:08:17 -0500

    Challenges work on specific  chapters and can be tested

--- app/src/main/java/com/example/udemyjavacourse/udemy_section_five/UdemySectionFiveViewHolder.java
+++ app/src/main/java/com/example/udemyjavacourse/udemy_section_five/UdemySectionFiveViewHolder.java
@@ -1,8 +1,9 @@
 package com.example.udemyjavacourse.udemy_section_five;
 
 import android.content.Context;
+import android.text.InputType;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -12,11 +13,14 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.udemyjavacourse.R;
 import com.example.udemyjavacourse.enums.Chapter;
 
-public class UdemySectionFiveViewHolder extends RecyclerView.ViewHolder {
+public class UdemySectionFiveViewHolder extends RecyclerView.ViewHolder implements UdemySectionFiveHolderInterface {
 
     public UdemySectionFivePresenter presenter;
     public LinearLayout chapterLayout;
     public Chapter chapter;
+    EditText methodOverloadChallengeInput;
+    TextView methodOverloadChallengeResult;
+    EditText methodOverloadChallengeFeet;
     Context context;
     TextView name;
     public UdemySectionFiveViewHolder(@NonNull View itemView) {
@@ -32,16 +36,59 @@ public class UdemySectionFiveViewHolder extends RecyclerView.ViewHolder {
 
     }
 
+    public void bind(UdemySectionFivePresenter presenter) {
+        this.presenter = presenter;
+        presenter.setUdemySectionViewHolder(this);
+    }
+
     public void setChallengeVisibility(Chapter chapter, View itemView) {
         switch (chapter) {
             case INTRODUCTION -> itemView.findViewById(R.id.section_five_challenges_layout).setVisibility(View.GONE);
-            case METHOD_OVERLOADING -> itemView.findViewById(R.id.section_five_challenges_layout).setVisibility(View.VISIBLE);
+            case METHOD_OVERLOADING -> setUpMethods();
+
         }
     }
 
+    public void setUpMethods() {
+        itemView.findViewById(R.id.section_five_challenges_layout).setVisibility(View.VISIBLE);
 
-
-    public void hideView() {
-
+        bindViews(itemView);
+        setInputTypes();
+        setEditTextListeners();
     }
+
+    private void setEditTextListeners() {
+        presenter.setEditListener(methodOverloadChallengeInput);
+        presenter.setEditListener(methodOverloadChallengeFeet);
+    }
+
+    private void bindViews(View itemView) {
+        methodOverloadChallengeInput = itemView.findViewById(R.id.method_overloading_challenge);
+        methodOverloadChallengeResult = itemView.findViewById(R.id.method_overloading_challenge_result);
+        methodOverloadChallengeFeet = itemView.findViewById(R.id.method_overloading_feet);
+    }
+
+
+
+    private void setInputTypes() {
+        methodOverloadChallengeInput.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
+        methodOverloadChallengeFeet.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
+    }
+
+
+    @Override
+    public int[] getMethodOverloadChallengeParameters() {
+
+        if (methodOverloadChallengeInput.getText().toString().equals("") || methodOverloadChallengeFeet.getText().toString().equals("")) {
+            return new int[] {0, 0};
+        }
+
+        return new int[]{Integer.parseInt(methodOverloadChallengeFeet.getText().toString()), Integer.parseInt(methodOverloadChallengeInput.getText().toString())};
+    }
+
+    @Override
+    public void setMethodOverloadChallengeInput(String text) {
+        methodOverloadChallengeResult.setText(text + " cm");
+    }
+
 }
